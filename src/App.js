ba3f84a0d90aef4a43f82d44899fb9808ca99c73
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-29 11:39:07 -0500

    function implement

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 //import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Header from './component/Header'
+//import Header from './component/Header'
 
 function App() {
-    return ( <
-        React.Fragment >
-        <
-        div className = "App" >
-        <
-        h1 className = 'btn danger' > This is first react project < /h1> < /
-        div >
-        <
-        Header > < /Header>  <
-        /React.Fragment>
-    );
+    return ( 
+        <div>
+            <Person name="Nasim Ahmed" job="Development"></Person>
+            <Person name="Development"></Person>
+        </div>
+    
+    )
 }
 
+function Person({name,job}){
+    return(<div>
+        <h1>{name}</h1>
+        <h2>profession:{job}</h2>
+        </div>
+        
+    )
+}
 export default App;
---
+++ src/component/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <div>
+            <h1>this is the footer section</h1>
+        </div>
+    );
+};
+
+export default footer;
