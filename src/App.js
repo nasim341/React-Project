ba206cc114492bcdad86997e96f79e563de50241
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-31 06:10:09 -0500

    simple json data load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
 //import Header from './component/Header'
 
 /* const singerStyle = {
@@ -12,28 +13,57 @@ function App() {
         <div className='App'>
             <h1 style={{color:'red'}}>React</h1>
             
-            <Counter></Counter>
+            {/* <Counter></Counter> */}
+            <ExternalUsers></ExternalUsers>
             
             
         </div>
     );
 }
-
-function Counter(){
-    const [count,setCount] = useState(0);
-    const incraeseCount=()=>{
-        setCount(count+1);
+function ExternalUsers(props){
+    const [users,setUsers]=useState([])
+    
+    useEffect(() => {
+        loadUsers()
+    },[])
+    const loadUsers = async()=>{
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+        console.log(data);
+        setUsers(data);
     }
-    const decreaseCount=()=>{
-        setCount(count-1);
-    }
-   
     return(
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={incraeseCount} className='btn btn-primary'>Increase</button>
-            <button onClick={decreaseCount} className='btn btn-'>Decrease</button>
+            <h1>User Information:{users.length}</h1>
+            {
+                users.map(user => <User user={user}></User>)
+            }
         </div>
     )
 }
+function User ({user}){
+    return(
+        <div className='person'>
+            <h1>Name:{user.name}</h1>
+            <p>Email:{user.email}</p>
+            <p>Username:{user.username}</p>
+        </div>
+    )
+}
+// function Counter(){
+//     const [count,setCount] = useState(0);
+//     const incraeseCount=()=>{
+//         setCount(count+1);
+//     }
+//     const decreaseCount=()=>{
+//         setCount(count-1);
+//     }
+   
+//     return(
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={incraeseCount} className='btn btn-primary'>Increase</button>
+//             <button onClick={decreaseCount} className='btn btn-'>Decrease</button>
+//         </div>
+//     )
+// }
 export default App;
